d9cb1e9f58d1bbd24176bddce1ef6cd07745e6a5
Natacha <natacha.har@onja.org>
2020-07-11 14:27:51 +0300

    Added new post using string

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 console.log('it works');
 
-// Hiding form
+// When the user click hide form, the form is hidden
+//hideForm.addEventListener('click', ($event) => {
+    //if($event.target)
+    //newCard.style.display = 'none';
+   // hideForm.style.display = 'none';
+//});
 
-const hideForm = document.getElementById('show-form');
+//addPost.addEventListener('click', ($event) => {
+    //if($event.target)
+    //newCard.style.display = 'block';
+    //hideForm.style.display = 'block';
+//});
+
+
+const hideForm = document.getElementsByClassName('.hidden');
 const newCard = document.getElementById('form-card');
+const cardForm = document.querySelector('#form-card');
+const form = document.querySelector('form');
+const postTitle = document.querySelector('#postTitle');
+const submitButton = document.querySelector('.btn-primary');
+const postContent = document.querySelector('textarea');
+const postAuthor = document.querySelector('input');
 
-hideForm.addEventListener('click', ($event) => {
-    if($event.target)
-    newCard.style.display = 'none';
+postContent.classList.add('post-content');
+postAuthor[1].classList.add('author');
+
+
+submitButton.addEventListener('click', ($event) => {
+    $event.preventDefault();
+
+const myNewHtml = `
+<div class="card">
+    <div class="card-body">
+        <img src="https://picsum.photos/500/200">
+        <h5>${postTitle.value} by ${postAuthor.value}</h5>
+        <p>${postContent.value}</p>
+    </div>
+</div>
+`;
+
+cardForm.innerHTML += myNewHtml;
+
+form.reset();
 });
+
