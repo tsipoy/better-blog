2dc3271b3de86215ff47bf3641973eb3b6bd4d19
Natacha <natacha.har@onja.org>
2020-07-11 09:14:15 +0300

    The form was hidden

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('it works');
+
+// Hiding form
+
+const hideForm = document.getElementById('show-form');
+const newCard = document.getElementById('form-card');
+
+hideForm.addEventListener('click', ($event) => {
+    if($event.target)
+    newCard.style.display = 'none';
+});
