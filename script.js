625a79a80291c11f364634003757c3d472ffa649
Natacha <natacha.har@onja.org>
2020-07-13 09:22:00 +0300

    split()was used to count the words inside the textarea

--- script.js
+++ script.js
@@ -3,29 +3,33 @@ console.log('it works');
 // Grabbing elements
 
 const cardForm = document.querySelector('#post-list');
-console.log(cardForm);
 const form = document.querySelector('#post-form');
 const postTitle = document.querySelector('#postTitle');
 const submitButton = document.querySelector('.btn-primary');
 const postContent = document.querySelector('textarea');
 const postAuthor = document.querySelector('[name="postAuthor"]');
 const errorMessage = document.querySelector('#error-message');
-const hideForm = document.getElementById('show-form');
+const hideForm = document.querySelector('#show-form');
 const newCard = document.getElementById('form-card');
-
+const card = document.querySelector('.card');
 postContent.classList.add('post-content');
 
 submitButton.addEventListener('click', ($event) => {
     $event.preventDefault();
+    const date = new Date();
     const myNewHtml = `
     <div class="card">
         <div class="card-body">
             <img src="https://picsum.photos/500/200">
-            <h5>${postTitle.value} by ${postAuthor.value}</h5>
+            <h5>${postTitle.value} <small>by ${postAuthor.value}</small></h5>
             <p>${postContent.value}</p>
-        </div>
-        <div class="card-footer text-muted>
-            10/07/2020
+            <button type="button" class="btn btn-sm btn-light btn-delete">
+							Delete entry
+						</button>
+					</div>
+					<div class="card-footer text-muted">
+						${date}
+					</div>
         </div>
     </div>
     `;
@@ -35,7 +39,7 @@ submitButton.addEventListener('click', ($event) => {
 });
 
 postContent.addEventListener('input', ($event) => {
-    if($event.target.value.length < 20) {
+    if($event.target.value.split(" ").length < 20) {
         errorMessage.style.display = 'block';
     } else {
         errorMessage.style.display = 'none';
@@ -43,6 +47,12 @@ postContent.addEventListener('input', ($event) => {
 });
 
 hideForm.addEventListener('click', ($event) => {
-    if($event.target)
-    newCard.style.display = 'none';
+    
+    if($event.target === 'true') {
+    newCard.classList.add('block');
+    } else {
+        newCard.classList.add('hidden');
+        hideForm.textContent = 'show form'; 
+    
+}
 });
